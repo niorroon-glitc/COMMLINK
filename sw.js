c56ae1a135071ebe74b5350fda095bd9171adf81
niorroon-glitc <niorroon@gmail.com>
2025-12-19 23:23:11 -0500

    feat: Update service worker cache version
    Increment service worker cache version and add new dependency for React 18 client rendering.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'commlink-v9.0-fix';
+const CACHE_NAME = 'commlink-v12.0-clean';
 const ASSETS = [
   './',
   './index.html',
@@ -7,6 +7,7 @@ const ASSETS = [
   'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js',
   'https://esm.sh/react@18.2.0',
   'https://esm.sh/react-dom@18.2.0',
+  'https://esm.sh/react-dom@18.2.0/client',
   'https://esm.sh/react@18.2.0/jsx-runtime'
 ];
 
